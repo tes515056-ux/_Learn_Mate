fa13c229d1d04605add45d19742ccbbc9bf6599c
tes515056-ux <tes515056@gmail.com>
2026-06-12 15:28:27 +0630

    update

--- dblink.php
+++ dblink.php
@@ -1,14 +1,13 @@
 <?php
+$servername="localhost";
+$username="root";
+$password="";
+$database="learnmate";
 
-$servername = "sql204.infinityfree.com";
-$username   = "if0_42155087";
-$password   = "nvluniversity1";
-$database   = "if0_42155087_learnmate";
 
-$conn = mysqli_connect($servername, $username, $password, $database);
-
-if (!$conn) {
-    die("Error connecting to database: " . mysqli_connect_error());
+$conn=mysqli_connect($servername,$username,$password,$database);
+if(!$conn)
+{
+    die("Error to connect :".mysqli_connect_error());
 }
-
 ?>
